dc3918c189d40a60e6d3e4d1c319f29c67c120f1
TheInfinity007 <ramchandra71999@gmail.com>
2020-04-10 22:47:31 +0530

    add all the files

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+$("ul").on("click", "li",  function(){
+	$(this).toggleClass("completed");
+});
+
+//click on x to delete Todo
+$("ul").on("click", "span",  function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		if(todoText !== ''){
+			var txt = "<li> <span> <i class='fas fa-trash'></i> </span>" + todoText + "</li>";
+			$("ul").append(txt); 
+			$(this).val("");	
+		}
+	}
+});
+
+$(".fa-plus").on("click", function(){
+	$("input[type='text']").fadeToggle();
+})
